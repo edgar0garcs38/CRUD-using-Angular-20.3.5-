7e42f4b4205248271539dddfec93e676ca31da9f
Eduardo <fmendoza094@gmail.com>
2025-11-13 18:57:04 -0500

    New styles were added to the create interface and the index.

--- src/app/post/index/index.ts
+++ src/app/post/index/index.ts
@@ -7,7 +7,7 @@ import { RouterModule } from "@angular/router";
   selector: 'app-index',
   imports: [RouterModule],
   templateUrl: './index.html',
-  styleUrl: './index.css'
+  styleUrls: ['./index.css']
 })
 export class Index {
 
